170c2ec82ea55b7e1949c12732b669e99d2e3fd2
Mina  Faried <mina.el.dihabi3.6.9@gmail.com>
2022-12-19 13:40:29 +0200

    linear search cpp

--- b-Searching_Algorithms/a-Linear_search/linear_search.cpp
+++ b-Searching_Algorithms/a-Linear_search/linear_search.cpp
@@ -33,11 +33,59 @@ using namespace std;
                              << endl;
 #define full(arr, val) memset(arr, val, sizeof(arr) - 1)
 
-int linearSearch(int arr[], int size, int num)
+///---------------------------------------------------------------------------------------------------------
+
+// ? ITERATIVE WAY
+int linearSearchIterative(int arr[], int size, int num)
 {
-    
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == num)
+        {
+            return i; // the index of founded num
+        }
+    }
+
+    return -1; //-1 means not found in the array
 }
 
+///==================================================================
+//? RECURSIVE WAY
+// EASY WAY
+int linearSearchRecursive1(int arr[], int size, int num, int current = 0)
+{
+    if (current == size)
+        return -1;
+
+    if (arr[current] == num)
+        return current;
+
+    return linearSearchRecursive1(arr, size, num, current + 1);
+}
+
+// SMART WAY
+int linearSearchRecursive2(int arr[], int size, int num)
+{
+    if (size < 0)
+        return -1;
+
+    if (arr[size] == num)
+        return size;
+
+    return linearSearchRecursive2(arr, size - 1, num);
+}
 int main()
 {
+    int arr[7] = {2, 8, 9, 1, 5, 0, 7};
+
+    LABEL(1, "ITERATIVE WAY")
+    cout << "5 found at index : " << linearSearchIterative(arr, 7, 5) << endl;
+
+    BREAK; //-----------------------------------
+
+    LABEL(2, "RECURSIVE EASY WAY")
+    cout << "5 found at index : " << linearSearchRecursive1(arr, 7, 5) << endl;
+
+    LABEL(2, "RECURSIVE SMART WAY")
+    cout << "5 found at index : " << linearSearchRecursive2(arr, 7, 5) << endl;
 }
